bfa25309f739c1adb102d1eaaf71e363b8d452b8
joel <allyouneed3002@gmail.com>
2022-12-17 18:51:33 +0900

    ssd_backbone

--- model/backbone/vgg.py
+++ model/backbone/vgg.py
@@ -1,88 +1,122 @@
-import sys
-sys.path.append('')
 import torch
 import torch.nn as nn
-import torchvision
-from model.backbone.layer.common import Conv, MP
 
-class VGG(nn.Module):
+from model.backbone.common import MP, Conv
 
-    def __init__(self, in_c, info, extra_info, init_weight=True):
-        super(VGG, self).__init__()
-        self.in_c= in_c
-        self.vgg_layer= self.create_conv_layer(info)
-        self.add_layer= nn.Sequential(
-            nn.Conv2d(in_channels= 512, out_channels= 1024, kernel_size=3, padding=6, dilation= 6),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=1024, out_channels= 1024, kernel_size= 1),
+class Cretae_Layer(nn.Module):
+    def __init__(self, cfg, in_c):
+        super(Cretae_Layer, self).__init__()
+        self.layer= self._create_layer(cfg, in_c)
+
+    def forward(self, x):
+        x= self.layer(x)
+        return x
+
+    def _create_layer(self, cfg, in_c):
+        layers= list()
+        in_c= in_c
+        for x in cfg:
+            if type(x) == int:
+                out_c= x
+                layers+= [Conv(in_c, out_c, k= 3, s= 1, p= None)]
+                in_c= x
+            elif x == 'M':
+                layers+= [MP(2)]
+        return nn.Sequential(*layers)
+
+
+class VGGBase(nn.Module):
+    def __init__(self):
+        super(VGGBase, self).__init__()
+        cfg_1= [64, 64, 'M']
+        cfg_2= [128, 128, 'M']
+        cfg_3= [256, 256, 256, 'M']
+        cfg_4= [512, 512, 512, 'M']
+        cfg_5= [512, 512, 512]
+        cfg_6= [512, 1024]
+        cfg_7= [1024, 1024]
+
+        self.conv1= Cretae_Layer(cfg_1, 3)
+        self.conv2= Cretae_Layer(cfg_2, cfg_1[0])
+        self.conv3= Cretae_Layer(cfg_3, cfg_2[0])
+        self.conv4= Cretae_Layer(cfg_4, cfg_3[0])
+        self.conv5= Cretae_Layer(cfg_5, cfg_4[0])
+        self.pool5= nn.MaxPool2d(3, 1, 1)
+        self.conv6= Cretae_Layer(cfg_6, cfg_5[0])
+        self.conv7= Cretae_Layer(cfg_7, cfg_7[0])
+
+
+    def forward(self, x):
+        conv1= self.conv1(x)
+        conv2= self.conv2(conv1)
+        conv3= self.conv3(conv2)
+        conv4= self.conv4(conv3)
+        conv5= self.conv5(conv4)
+        conv5= self.pool5(conv5)
+        conv6= self.conv6(conv5)
+        conv7= self.conv7(conv6)
+
+        return conv4, conv7
+
+class AuxiliaryConv(nn.Module):
+    def __init__(self):
+        super(AuxiliaryConv, self).__init__()
+        cfg_8= [1024, 256, 512]
+        cfg_9= [512, 128, 256]
+        cfg_10= [256, 128, 256]
+        cfg_11= [256, 128, 256]
+
+        self.conv8= nn.Sequential(
+            nn.Conv2d(cfg_8[0], cfg_8[1], 1, 1, 0),
+            nn.Conv2d(cfg_8[1], cfg_8[2], 3, 2, 1),
             nn.ReLU()
         )
 
-        self.extra_layer= self.create_extra_layers(extra_info)
-        
-        if init_weight:
-            self._initialize_weights()
-    
-    def forward(self,x):
-        x= self.vgg_layer(x)
-        x= self.add_layer(x)
-        x= self.extra_layer(x)
-        return x
-    
-    def create_conv_layer(self, info):
-        layers= list()
-        in_c= self.in_c
-        
-        for x in info:
-            if type(x) == int:
-                out_c= x
-                layers+= [Conv(in_c= in_c, out_c= out_c, k= 3, s= 1, p= None, g= 1, act= True)]
-                in_c= x   
-            elif x == 'M':
-                layers+= [MP(k=2)]
-        
-        return nn.Sequential(*layers)
+        self.conv9= nn.Sequential(
+            nn.Conv2d(cfg_9[0], cfg_9[1], 1, 1, 0),
+            nn.Conv2d(cfg_9[1], cfg_9[2], 3, 2, 1),
+            nn.ReLU()
+        )
 
-    def create_extra_layers(self, extra_info):
-        in_c= 1024
-        layers= list()
+        self.conv10= nn.Sequential(
+            nn.Conv2d(cfg_10[0], cfg_10[1], 1, 1, 0),
+            nn.Conv2d(cfg_10[1], cfg_10[2], 3, 1, 0),
+            nn.ReLU()
+        )
 
-        layers += [nn.Conv2d(in_c, extra_info[0], kernel_size=1)]
-        layers += [nn.Conv2d(extra_info[0], extra_info[1], kernel_size=3, stride=2, padding=1)]
-        layers += [nn.Conv2d(extra_info[1], extra_info[2], kernel_size=1)]
-        layers += [nn.Conv2d(extra_info[2], extra_info[3], kernel_size=3, stride=2, padding=1)]
-        layers += [nn.Conv2d(extra_info[3], extra_info[4], kernel_size=1)]
-        layers += [nn.Conv2d(extra_info[4], extra_info[5], kernel_size=3)]
-        layers += [nn.Conv2d(extra_info[5], extra_info[6], kernel_size=1)]
-        layers += [nn.Conv2d(extra_info[6], extra_info[7], kernel_size=3)]        
+        self.conv11= nn.Sequential(
+            nn.Conv2d(cfg_11[0], cfg_11[1], 1, 1, 0),
+            nn.Conv2d(cfg_11[1], cfg_11[2], 3, 1, 0),
+            nn.ReLU()
+        )
 
-        return nn.Sequential(*layers)
-    
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-                if m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.constant_(m.bias, 0)
-                
-    def _load_pretrained_weight(self):
-        weight= torchvi
+    def forward(self, conv7):
+        conv8= self.conv8(conv7)
+        conv9= self.conv9(conv8)
+        conv10= self.conv10(conv9)
+        conv11= self.conv11(conv10)
 
-def backbone():
-    vgg16_info = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M']
-    extra_info= [256, 512, 128, 256, 128, 256, 128, 256]
-    net= VGG(3, vgg16_info, extra_info, True)
-    return net
+        return conv8, conv9, conv10, conv11
+
+class SSD_BackBone(nn.Module):
+    def __init__(self):
+        super(SSD_BackBone, self).__init__()
+        self.vgg= VGGBase()
+        self.extra= AuxiliaryConv()
+
+    def forward(self, x):
+        conv4, conv7= self.vgg(x)
+        conv8, conv9, conv10, conv11= self.extra(conv7)
+
+        return conv4, conv7, conv8, conv8, conv9, conv10, conv11
 
 if __name__ == '__main__':
-    net= backbone()
-    x= torch.rand((1, 3, 300, 300))
-    print(net)
-    output= net(x)
-    print(output.size())
+    model = SSD_BackBone()
+    output= torch.randn((1, 3, 300, 300))
+    print(model)
+    conv4, conv7, conv8, conv8, conv9, conv10, conv11= model(output)
+
+
+
+
+
